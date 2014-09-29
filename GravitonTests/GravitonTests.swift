e6aa3a86e851d32a0b6e70a97644739e161c3ca4
Karl Johan Krantz <kallekrantz@Karls-MacBook-Pro.local>
2014-09-29 23:46:53 +0200

    Initial Commit

---
+++ GravitonTests/GravitonTests.swift
@@ -0,0 +1,36 @@
+//
+//  GravitonTests.swift
+//  GravitonTests
+//
+//  Created by Karl Johan Krantz on 29/09/14.
+//  Copyright (c) 2014 Karl Johan Krantz. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class GravitonTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
